4a2e1f1d95f59c8228966a6e0185306c9a5ac53e
Sebastien34 <sebastienwendling@iteeweb.fr>
2024-01-30 23:47:57 +0100

    service modif

--- src/pages/components/service.tsx
+++ src/pages/components/service.tsx
@@ -4,7 +4,7 @@ export default function Service() {
   return (
     <>
       <section>
-        <div>
+        <div className="h-screen">
           <div className="bordure flex justify-center">
             <span></span>
             <span></span>
@@ -12,15 +12,95 @@ export default function Service() {
             <span></span>
             <h1 className="text-6xl">mes services</h1>
           </div>
-          <div>
-            <h1>app react/next.js</h1>
+          <div className="w-full sm:w-1/2">
+            <h1 className="flex justify-center">A voir pour le titre</h1>
           </div>
-          <div>
-            <div className="card w-1/3 h-dvh shadow-lg shadow-white">
-              <div className="card-body">
-                <h2 className="card-title">Site vitrine</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-              </div>
+          <div className="grid grid-cols-3 gap-4 divide-x divide-cyan-500 bg-gradient-to-b from-stone-800">
+            <div className="card-body">
+              <h3 className="card-title text-2xl">Site vitrine sur mesure</h3>
+              <ul>
+                <li>1 page d accueil</li>
+                <li>1 page produit ou service</li>
+                <li>1 page de contact</li>
+                <li>1 page entreprise et histoire</li>
+                <li>1 mention légales</li>
+                <li>1 page politique de confidentialité</li>
+              </ul>
+              <br />
+              <h4 className="text-2xl">La rédaction des pages</h4>
+              <br />
+              <p>
+                Je rédige l’intégralité de votre contenu pour le rendre
+                attractif et optimisé pour le référencement naturel. La formule
+                idéale pour attirer, qualifier et convertir vos prospects en
+                clients.
+              </p>
+              <br />
+              <h4 className="text-2xl">Pack référencement complet</h4>
+              <br />
+              <p>
+                Je crée un site web optimisé pour les moteurs de recherche et je
+                crée une stratégie de mots-clés sur mesure pour attirer du
+                trafic gratuit et qualifié sur le long terme.
+              </p>
+            </div>
+
+            <div className="card-body">
+              <h3 className="card-title">Site vitrine sur mesure</h3>
+              <ul>
+                <li>1 page d accueil</li>
+                <li>1 page produit ou service</li>
+                <li>1 page de contact</li>
+                <li>1 page entreprise et histoire</li>
+                <li>1 mention légales</li>
+                <li>1 page politique de confidentialité</li>
+              </ul>
+              <br />
+              <h4 className="text-2xl">La rédaction des pages</h4>
+              <br />
+              <p>
+                Je rédige l’intégralité de votre contenu pour le rendre
+                attractif et optimisé pour le référencement naturel. La formule
+                idéale pour attirer, qualifier et convertir vos prospects en
+                clients.
+              </p>
+              <br />
+              <h4 className="text-2xl">Pack référencement complet</h4>
+              <br />
+              <p>
+                Je crée un site web optimisé pour les moteurs de recherche et je
+                crée une stratégie de mots-clés sur mesure pour attirer du
+                trafic gratuit et qualifié sur le long terme.
+              </p>
+            </div>
+
+            <div className="card-body">
+              <h3 className="card-title">Site vitrine sur mesure</h3>
+              <ul>
+                <li>1 page d accueil</li>
+                <li>1 page produit ou service</li>
+                <li>1 page de contact</li>
+                <li>1 page entreprise et histoire</li>
+                <li>1 mention légales</li>
+                <li>1 page politique de confidentialité</li>
+              </ul>
+              <br />
+              <h4 className="text-2xl">La rédaction des pages</h4>
+              <br />
+              <p>
+                Je rédige l’intégralité de votre contenu pour le rendre
+                attractif et optimisé pour le référencement naturel. La formule
+                idéale pour attirer, qualifier et convertir vos prospects en
+                clients.
+              </p>
+              <br />
+              <h4 className="text-2xl">Pack référencement complet</h4>
+              <br />
+              <p>
+                Je crée un site web optimisé pour les moteurs de recherche et je
+                crée une stratégie de mots-clés sur mesure pour attirer du
+                trafic gratuit et qualifié sur le long terme.
+              </p>
             </div>
           </div>
         </div>
