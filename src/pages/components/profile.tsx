bb86e5dcbcdee2fd674810f312ff445992380c07
Sebastien34 <sebastienwendling@iteeweb.fr>
2024-02-02 16:00:45 +0100

    profile modif

--- src/pages/components/profile.tsx
+++ src/pages/components/profile.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+//import Card from "./card";
 
 export default function Profile() {
   return (
@@ -10,27 +11,42 @@ export default function Profile() {
               SecureWeb Creation
             </h1>
           </div>
-          <div className="bg-gradient-to-b from-stone-800 text-center">
-            <h2 className="text-5xl md:text-6xl lg:text-6xl font-bold mt-4 mb-10">
-              Développeur Web, création de site web.
-            </h2>
-            <div className="flex flex-col sm:flex-row items-center justify-center sm:grid mt-10 md:grid-cols-2 gap-4 md:flex justify-center h-38">
-              <div className="flex-shrink-0">
-                <Image
-                  src="/logos/react-logos.png"
-                  width="128"
-                  height="128"
-                  alt="react-logo"
-                  className=""
-                />
-              </div>
+          <div className="bg-gradient-to-b from-stone-800">
+            <div className=" text-center">
+              <h2 className="text-5xl md:text-6xl lg:text-6xl font-bold mt-4 mb-10">
+                Développeur Web, création de site web.
+              </h2>
+              <div className="flex flex-col sm:flex-row items-center justify-center sm:grid mt-10 md:grid-cols-2 gap-4 md:flex justify-center h-38">
+                <div className="flex-shrink-0">
+                  <Image
+                    src="/logos/react-logos.png"
+                    width="128"
+                    height="128"
+                    alt="react-logo"
+                    className=""
+                  />
+                </div>
 
-              <div className="flex-shrink-0">
+                <div className="flex-shrink-0">
+                  <Image
+                    src="/logos/wordpress.png"
+                    width="220"
+                    height="220"
+                    alt="wordpress-logo"
+                    className=""
+                  />
+                </div>
+              </div>
+              <div className="h-35 divider divider-info" />
+              <h2 className="text-4xl md:text-4xl lg:text-6xl font-bold mt-4 mb-10">
+                Des sites web responsive.
+              </h2>
+              <div className="flex flex-col sm:flex-row items-center justify-center sm:grid mt-10 md:grid-cols-2 gap-4 md:flex justify-center h-38">
                 <Image
-                  src="/logos/wordpress.png"
-                  width="220"
-                  height="220"
-                  alt="wordpress-logo"
+                  src="/logos/responsiveDessign.png"
+                  width="500"
+                  height="500"
+                  alt="responsive-design"
                   className=""
                 />
               </div>
